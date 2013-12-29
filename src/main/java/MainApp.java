e6859d04a8894ef41bd8038e439cc44204b6dacf
kit <tony_npu_ta@yahoo.com>
2013-12-29 14:04:47 -0800

    3rd

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,6 @@
 
 public class MainApp {
 	public static void main(String[] args) {	   
-	      System.out.println("The Sum:" + Add.add(9, 1));
+	      System.out.println("The Sum:" + Add.add(9, 2));
 	   }
 }
